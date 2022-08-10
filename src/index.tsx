59c6041e666fa9d8121bc32a92eeb142126c6eff
Alan Rente <alan.rente@iksolution.com.br>
2022-08-10 02:21:03 -0300

    fix: rename page ListStudents to Students

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import axios from 'axios';
+import { Students } from 'pages/Students';
 
 axios.defaults.baseURL = process.env.REACT_APP_API;
 
@@ -11,6 +11,6 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Students />
   </React.StrictMode>
 );
